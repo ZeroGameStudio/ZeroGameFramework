363b2e5758b4b64d1cfdfce680ee94650889dfa1
Lin7Han <941532182@qq.com>
2025-01-10 09:46:16 +0800

    Update ZeroGameFrameworkExtensionChannels.cpp

--- Source/ZeroGameFrameworkRuntime/Private/Extension/ZeroGameFrameworkExtensionChannels.cpp
+++ Source/ZeroGameFrameworkRuntime/Private/Extension/ZeroGameFrameworkExtensionChannels.cpp
@@ -2,7 +2,7 @@
 
 #include "Extension/ZeroGameFrameworkExtensionChannels.h"
 
-UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_ExtensionChannel_ActorInitializeComponents, "ZeroGameFramework.ExtensionChannel.ActorInitializeComponents", "Extension channel registered in Actor::InitializeComponents.");
-UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_ExtensionChannel_ActorBeginPlay, "ZeroGameFramework.ExtensionChannel.ActorBeginPlay", "Extension channel registered in Actor::BeginPlay.");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_ExtensionChannel_ActorInitializeComponents, "ZeroExtensionSystem.Channel.ActorInitializeComponents", "Extension channel registered in Actor::InitializeComponents.");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_ExtensionChannel_ActorBeginPlay, "ZeroExtensionSystem.Channel.ActorBeginPlay", "Extension channel registered in Actor::BeginPlay.");
 
 
